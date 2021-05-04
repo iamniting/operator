f001819a5918f85242390c30c03b34df7ac9ccbe
Nitin Goyal <nigoyal@redhat.com>
2021-05-04 17:12:23 +0530

    Define api
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- api/v1/appconfig_types.go
+++ api/v1/appconfig_types.go
@@ -28,14 +28,17 @@ type AppConfigSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of AppConfig. Edit AppConfig_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	DesiredCount int    `json:"desiredCount,omitempty"`
+	Command      string `json:"command,omitempty"`
+	Image        string `json:"image,omitempty"`
 }
 
 // AppConfigStatus defines the observed state of AppConfig
 type AppConfigStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	CurrentCount int `json:"currentCount,omitempty"`
 }
 
 // +kubebuilder:object:root=true
